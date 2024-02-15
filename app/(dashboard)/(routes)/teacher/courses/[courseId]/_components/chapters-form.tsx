65bff3aae78c7d8a8f60d14f5ecc37f9c91cca70
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-02-15 07:08:53 +0100

    implement chapter reorder functionnality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -14,12 +14,13 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { PlusCircleIcon } from "lucide-react";
+import { Loader2, PlusCircleIcon } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Chapter, Course } from "@prisma/client";
+import ChaptersList from "./chapters-list";
 
 interface ChaptersFormProps {
     initialData: Course & { chapters: Chapter[] };
@@ -65,8 +66,29 @@ const ChaptersForm = ({
         }
     }
 
+    const onReorder = async (updateData: { id: string; position: number }[]) => {
+        try {
+            setIsUpdating(true);
+
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                list: updateData
+            });
+            toast.success("Chapitres réorganisés avec succès");
+            router.refresh();
+        } catch {
+            toast.error("Quelque chose s'est mal passé");
+        } finally {
+            setIsUpdating(false);
+        }
+    }
+
     return (
-        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+        <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+            {isUpdating && (
+                <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+                    <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+                </div>
+            )}
             <div className="font-medium flex items-center justify-between">
                 Chapitres du cours
                 <Button 
@@ -120,7 +142,11 @@ const ChaptersForm = ({
                     !initialData.chapters.length && "text-slate-500 italic"
                 )}>
                     {!initialData.chapters.length && "Aucun chapitre"}
-                    {/* TODO : Add a list of chapters */}
+                    <ChaptersList 
+                        onEdit={() => {}}
+                        onReorder={onReorder}
+                        items={initialData.chapters || []}
+                    />
                 </div>
             )}
             {!isCreating && (
