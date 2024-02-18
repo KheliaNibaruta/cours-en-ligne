5aa15f1931450c88b17c1195bd7c85909174d94b
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-02-18 19:14:23 +0100

    implement course publish action

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,8 @@ import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import AttachmentForm from "./_components/attachment-form";
 import ChaptersForm from "./_components/chapters-form";
+import Banner from "@/components/banner";
+import CourseActions from "./_components/course-actions";
 
 const Coursepage = async ({
     params
@@ -65,97 +67,112 @@ const Coursepage = async ({
     const totalFields = requiredFields.length;
     const completedFields = requiredFields.filter(Boolean).length;
 
-    const completionText = `(${completedFields} / ${totalFields})`
+    const completionText = `(${completedFields} / ${totalFields})`;
+
+    const isComplete = requiredFields.every(Boolean);
 
   return (
-    <div className="p-6">
-        <div className="flex items-center justify-between">
-            <div className="flex flex-col gap-y-2">
-                <h1 className="text-2xl font-medium">
-                    Paramètres du cours
-                </h1>
-                <span className="text-sm text-slate-500">
-                    Compléter tous les champs avant de publier le cours {completionText}
-                </span>
+    <>
+    {!course.isPublished && (
+      <Banner 
+        variant="warning"
+        label="Ce cours n'est pas publié!!!"
+      />
+    )}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">
+                        Paramètres du cours
+                    </h1>
+                    <span className="text-sm text-slate-500">
+                        Compléter tous les champs avant de publier le cours {completionText}
+                    </span>
+                </div>
+                <CourseActions 
+                  disabled={!isComplete}
+                  courseId={params.courseId}
+                  isPublished={course.isPublished}
+              />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge
+                            icon={LayoutDashboard}
+                        />
+                        <h2 className="text-xl">
+                            Personnaliser votre cours
+                        </h2>
+                    </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <DescriptionForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <ImageForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <CategoryForm
+                        initialData={course}
+                        courseId={course.id}
+                        options={categories.map((category) => ({
+                            label: category.name,
+                            value: category.id,
+                        }))}
+                    />
+                </div>
+                <div className="space-y-6">
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge 
+                                icon={ListChecks}
+                            />
+                            <h2 className="text-xl">
+                                Chapitres du cours
+                            </h2>
+                        </div>
+                        <ChaptersForm
+                            initialData={course}
+                            courseId={course.id}
+                        />
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge 
+                                icon={CircleDollarSign}
+                            />
+                            <h2 className="text-xl">
+                                Vendre votre cours
+                            </h2>
+                        </div>
+                        <PriceForm 
+                            initialData={course}
+                            courseId={course.id}
+                        />
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge 
+                                icon={File}
+                            />
+                            <h2 className="text-xl">
+                                Pièces jointes
+                            </h2>
+                        </div>
+                        <AttachmentForm
+                            initialData={course}
+                            courseId={course.id}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-            <div>
-                <div className="flex items-center gap-x-2">
-                    <IconBadge
-                        icon={LayoutDashboard}
-                    />
-                    <h2 className="text-xl">
-                        Personnaliser votre cours
-                    </h2>
-                </div>
-                <TitleForm
-                    initialData={course}
-                    courseId={course.id}
-                />
-                <DescriptionForm
-                    initialData={course}
-                    courseId={course.id}
-                />
-                <ImageForm
-                    initialData={course}
-                    courseId={course.id}
-                />
-                <CategoryForm
-                    initialData={course}
-                    courseId={course.id}
-                    options={categories.map((category) => ({
-                        label: category.name,
-                        value: category.id,
-                    }))}
-                />
-            </div>
-            <div className="space-y-6">
-                <div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge 
-                            icon={ListChecks}
-                        />
-                        <h2 className="text-xl">
-                            Chapitres du cours
-                        </h2>
-                    </div>
-                    <ChaptersForm
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                </div>
-                <div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge 
-                            icon={CircleDollarSign}
-                        />
-                        <h2 className="text-xl">
-                            Vendre votre cours
-                        </h2>
-                    </div>
-                    <PriceForm 
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                </div>
-                <div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge 
-                            icon={File}
-                        />
-                        <h2 className="text-xl">
-                            Pièces jointes
-                        </h2>
-                    </div>
-                    <AttachmentForm
-                        initialData={course}
-                        courseId={course.id}
-                    />
-                </div>
-            </div>
-        </div>
-    </div>
+    </>
   )
 }
 
