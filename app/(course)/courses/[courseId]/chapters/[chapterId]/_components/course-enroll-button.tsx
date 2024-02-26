670633738dfc59709b4c81f2c6bb5d54f6d0513b
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-02-26 23:58:51 +0100

    add failing stripe integration

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollButtonProps {
     price: number;
@@ -12,10 +15,28 @@ const CourseEnrollButton = ({
     price,
     courseId,
 }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`)
+
+      window.location.assign(response.data.url);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <Button
-        size="sm"
-        className="w-full md:w-auto"
+      onClick={onClick}
+      disabled={isLoading}
+      size="sm"
+      className="w-full md:w-auto"
     >
         Acheter pour {formatPrice(price)}
     </Button>
