070de8dc9f14dd5163cad80411fa8ca72f4c5094
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-03-06 12:16:55 +0100

    implement teacher analytics dashboard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,8 +1,37 @@
+import { getAnalytics } from "@/actions/get-analytics";
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+import DataCard from "./_components/data-card";
+import Chart from "./_components/chart";
 
-const Analytics = () => {
+const Analytics = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    data,
+    totalRevenue,
+    totalSales,
+  } = await getAnalytics(userId);
   return (
-    <div>
-        Analytics page
+    <div className="p-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+          <DataCard 
+            label="Total des revenus"
+            value={totalRevenue}
+            shouldFormat
+          />
+          <DataCard 
+            label="Total des ventes"
+            value={totalSales}
+          />
+        </div>
+        <Chart 
+          data={data}
+        />
     </div>
   )
 }
