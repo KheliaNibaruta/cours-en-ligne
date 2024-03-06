beab1dd0372935131615119f812fc726a87b2a4b
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-03-06 12:48:44 +0100

    implement admin protection

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -2,6 +2,7 @@ import Mux from "@mux/mux-node";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import { isTeacher } from "@/lib/teacher";
 
 const mux = new Mux({
   tokenId: process.env.MUX_TOKEN_ID,
@@ -15,7 +16,7 @@ export async function DELETE(
   try {
     const { userId } = auth();
     
-    if (!userId) {
+    if (!userId || !isTeacher(userId)) {
       return new NextResponse("Accès non autorisé", { status: 401 });
   }
   
@@ -94,7 +95,7 @@ export async function PATCH(
         const { userId } = auth();
         const { isPublished, ...values} = await req.json();
 
-        if (!userId) {
+        if (!userId || !isTeacher(userId)) {
             return new NextResponse("Accès non autorisé", { status: 401 });
         }
 
--- components/navbarRoutes.tsx
+++ components/navbarRoutes.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { UserButton } from '@clerk/nextjs';
+import { UserButton, useAuth } from '@clerk/nextjs';
 import { usePathname, useRouter } from 'next/navigation';
 import { Button } from './ui/button';
+import { isTeacher } from '@/lib/teacher';
 import { LogOut } from 'lucide-react';
 import Link from 'next/link';
 import SearchInput from './search-input';
 
 const NavbarRoutes = () => {
+    const { userId } = useAuth();
   const pathname = usePathname();
   const router = useRouter();
 
@@ -29,13 +31,13 @@ const NavbarRoutes = () => {
                         Exit
                     </Button>
                 </Link>
-            ) : (
+            ) : isTeacher(userId) ? (
                 <Link href='/teacher/courses'>
                     <Button size="sm" variant="ghost">
                         Teacher mode
                     </Button>
                 </Link>
-            )}
+            ): null}
             <UserButton 
                 afterSignOutUrl='/'
             />
